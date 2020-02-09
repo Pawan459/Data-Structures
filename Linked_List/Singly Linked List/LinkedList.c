46b0eb86211bdfd47c0820686a4d6584ebb6dcda
pawan459 <pawan18092019@gmail.com>
2020-02-09 21:40:57 +0530

    Update LinkedList.c

--- Linked_List/Singly Linked List/LinkedList.c
+++ Linked_List/Singly Linked List/LinkedList.c
@@ -1,76 +1,88 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 
-typedef struct node{
+typedef struct node
+{
     int data;
     struct node *next;
 } node;
 
-typedef struct linkedList{
+typedef struct linkedList
+{
     struct node *head;
-}linkedList;
+} linkedList;
 
-node* insertAtLast(node *head, int data){
+node *insertAtLast(node *head, int data)
+{
     node *newnode;
     newnode = (node *)malloc(sizeof(node));
     newnode->data = data;
     newnode->next = NULL;
-    if(head == NULL){
+    if (head == NULL)
+    {
         return newnode;
     }
     node *currPtr = head;
 
-    while(currPtr->next != NULL){
+    while (currPtr->next != NULL)
+    {
         currPtr = currPtr->next;
     }
 
     currPtr->next = newnode;
-    
+
     return head;
 }
 
-void deleteNode(node **head, int dataKey){
-	node *prevNode = NULL;
-	node *currNode = *head;
-	
-	if(currNode->data == dataKey){
-		node *deleteNode = currNode;
-		currNode = currNode->next;
-		*head = currNode;
-		free(deleteNode);
-		return;
-	}
-	
-	while(currNode != NULL){
-		if(currNode->data == dataKey){
-			node *deleteNode = currNode;
-			prevNode->next = currNode->next;
-			free(deleteNode);
-			return;
-		}
-		
-		prevNode = currNode;
-		currNode = currNode->next;
-	}
-	
-	printf("Node with Data Not Found");
+void deleteNode(node **head, int dataKey)
+{
+    node *prevNode = NULL;
+    node *currNode = *head;
+
+    if (currNode->data == dataKey)
+    {
+        node *deleteNode = currNode;
+        currNode = currNode->next;
+        *head = currNode;
+        free(deleteNode);
+        return;
+    }
+
+    while (currNode != NULL)
+    {
+        if (currNode->data == dataKey)
+        {
+            node *deleteNode = currNode;
+            prevNode->next = currNode->next;
+            free(deleteNode);
+            return;
+        }
+
+        prevNode = currNode;
+        currNode = currNode->next;
+    }
+
+    printf("Node with Data Not Found");
 }
 
-void display(node *head){
+void display(node *head)
+{
     node *currPtr = head;
 
-    while(currPtr != NULL){
-        if(currPtr->next != NULL)
-            printf("%d -> ",currPtr->data);
+    while (currPtr != NULL)
+    {
+        if (currPtr->next != NULL)
+            printf("%d -> ", currPtr->data);
         else
-            printf("%d",currPtr->data);
-        
+            printf("%d", currPtr->data);
+
         currPtr = currPtr->next;
     }
 }
 
-int main() {
+int main()
+{
     linkedList ll;
     ll.head = NULL;
 
@@ -78,20 +90,20 @@ int main() {
     {
         ll.head = insertAtLast(ll.head, i);
     }
-    
+
     display(ll.head);
     printf("\n");
-    
+
     // Deletion from head
     deleteNode(&ll.head, 0);
     display(ll.head);
     printf("\n");
-    
+
     // Deletion from mid
     deleteNode(&ll.head, 5);
     display(ll.head);
     printf("\n");
-    
+
     // Deletion from last
     deleteNode(&ll.head, 9);
     display(ll.head);
